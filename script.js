40f91614fab4d08345828d3efc3d6b56b108a957
Yorbit <marco.95112@hotmail.it>
2022-06-22 15:00:40 +0200

    start js

---
+++ script.js
@@ -0,0 +1,19 @@
+let messagePrice;
+
+const outputNomeCognome = document.getElementById('Namehost');
+
+const outputType = document.getElementById('Type');
+
+const outputTrain = document.getElementById('Train');
+
+const outputPrice = document.getElementById('Price');
+
+const genTicket = document.querySelector('#button-gen');
+
+
+genTicket.addEventListener('click', function () {
+    console.log(document.getElementById('Km').value);
+    console.log(document.getElementById('Age').value);
+    console.log(document.getElementById('Nome-Cognome').value);
+
+}
